d2070123cc3a6f76cee2a34c57f24cff1ffac9dc
FordMcLeod <owen.mcleod1@gmail.com>
2019-03-07 12:33:07 -0700

    Completed free tests

--- freetests.py
+++ freetests.py
@@ -49,6 +49,7 @@ class ServerTestCase(unittest.TestCase):
                         , "Code not 200!")
         if (r.status_code == 200):
             self.assertTrue(len(r.data) > 5, "No data?")
+        print("PASSED TEST HELLO")
 
     def testUpdate(self):
         v = 'T'+str(random.randint(1,1000000))
@@ -64,6 +65,7 @@ class ServerTestCase(unittest.TestCase):
         r = self.app.get(('/entity/%s' % v))
         self.assertTrue(r.status_code == 200, "Code not 200!")
         self.assertTrue(json.loads(utf8(r.data)) == d, "D != r.data")
+        print("PASSED TEST UPDATE")
 
         
     def populateWorld(self):
@@ -91,11 +93,6 @@ class ServerTestCase(unittest.TestCase):
         newworld = json.loads(utf8(r.data))
         for key in self.world:
             self.assertTrue(self.world[key]  == newworld[key], "Key %s" % key)
-
-
-        
-        
-        
 
 if __name__ == '__main__':
     unittest.main()
--- server.py
+++ server.py
@@ -81,24 +81,26 @@ def update(entity):
     '''update the entities via this interface'''
     print("UPDATE WORLD CALLED ON entity: %s"%entity)
     updatejson = flask_post_json()
-    print(updatejson)
-    return flask.jsonify(updatejson)
+    for key,val in updatejson.items():
+        myWorld.update(entity,key,val)
+    print("CURRENT SERVER SIDE OF ENTITY:" + str(myWorld.get(entity)))
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return flask.jsonify(myWorld)
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return json.dumps(myWorld.get(entity))
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
-    return None
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
